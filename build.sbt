da6ff6d40305d5208b4949a041068a5b42c0c8ae
zdoonio <dominik.zdunczyk@gmail.com>
2019-04-12 20:45:27 +0200

    first commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "scala-bash"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
---
+++ src/main/scala/com/zedd2dev/scala/filesystem/Filesystem.scala
@@ -0,0 +1,17 @@
+package com.zedd2dev.scala.filesystem
+
+import java.util.Scanner
+
+/**
+  * Created by Dominik Zduńczyk on 12.04.19.
+  */
+object Filesystem extends App {
+
+  val scanner = new Scanner(System.in)
+
+  while(true) {
+    print("$ ")
+    println(scanner.nextLine())
+  }
+
+}
