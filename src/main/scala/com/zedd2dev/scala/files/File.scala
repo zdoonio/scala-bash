f9a5a18b83c7f7fa7d5a0a1ba7a02e34ef22d99b
zdoonio <dominik.zdunczyk@gmail.com>
2019-04-15 21:02:28 +0200

    feature(filesystem): implemented echo command

--- src/main/scala/com/zedd2dev/scala/files/File.scala
+++ src/main/scala/com/zedd2dev/scala/files/File.scala
@@ -18,6 +18,13 @@ class File(override val parentPath: String, override val name: String, contents:
   def isFile: Boolean = true
 
   override def getType: String = "File"
+
+  def setContents(newContents: String): File = {
+    new File(parentPath, name, newContents)
+  }
+
+  def appendContents(newContents: String): File =
+    setContents(contents + "\n" + newContents)
 }
 
 object File {
