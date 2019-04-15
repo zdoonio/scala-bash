7761928b1202c175ee930d6c504940252d9dc2ad
zdoonio <dominik.zdunczyk@gmail.com>
2019-04-15 19:49:42 +0200

    feature(filesystem): added rm command

--- src/main/scala/com/zedd2dev/scala/commands/Command.scala
+++ src/main/scala/com/zedd2dev/scala/commands/Command.scala
@@ -18,6 +18,7 @@ object Command {
   val PWD = "pwd"
   val TOUCH = "touch"
   val CD = "cd"
+  val RM = "rm"
 
   def emptyCommand: Command = new Command {
     override def apply(state: State): State = state
@@ -40,11 +41,14 @@ object Command {
     } else if(PWD.equals(tokens(0))) {
       new Pwd
     } else if(TOUCH.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(MKDIR)
+      if (tokens.length < 2) incompleteCommand(TOUCH)
       else new Touch(tokens(1))
     } else if (CD.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(MKDIR)
+      if (tokens.length < 2) incompleteCommand(CD)
       else new Cd(tokens(1))
+    } else if (RM.equals(tokens(0))) {
+      if (tokens.length < 2) incompleteCommand(RM)
+      else new Rm(tokens(1))
     }
     else
       new UnknownCommand
